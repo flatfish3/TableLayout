cb0a88911142e64fa17d78df7d6fd9122ac55524
steven <steven@steven-PC>
2012-10-30 12:32:09 +0900

    ver3.0

--- gen/com/example/tablelayout/R.java
+++ gen/com/example/tablelayout/R.java
@@ -23,31 +23,63 @@ public final class R {
         public static final int button14=0x7f070009;
         public static final int button15=0x7f07000e;
         public static final int button16=0x7f070013;
+        public static final int button17=0x7f070015;
+        public static final int button18=0x7f070016;
+        public static final int button19=0x7f070017;
         public static final int button2=0x7f070002;
+        public static final int button20=0x7f070018;
+        public static final int button21=0x7f07001a;
+        public static final int button22=0x7f07001b;
+        public static final int button23=0x7f07001c;
+        public static final int button24=0x7f07001d;
+        public static final int button25=0x7f07001e;
+        public static final int button26=0x7f070020;
+        public static final int button27=0x7f070021;
+        public static final int button28=0x7f070022;
+        public static final int button29=0x7f070023;
         public static final int button3=0x7f070003;
+        public static final int button30=0x7f070024;
+        public static final int button31=0x7f070026;
+        public static final int button32=0x7f070027;
+        public static final int button33=0x7f070028;
+        public static final int button34=0x7f070029;
+        public static final int button35=0x7f07002a;
+        public static final int button36=0x7f07002c;
+        public static final int button37=0x7f07002d;
+        public static final int button38=0x7f07002e;
+        public static final int button39=0x7f07002f;
         public static final int button4=0x7f070006;
+        public static final int button40=0x7f070030;
         public static final int button5=0x7f070007;
         public static final int button6=0x7f070008;
         public static final int button7=0x7f07000b;
         public static final int button8=0x7f07000c;
         public static final int button9=0x7f07000d;
-        public static final int menu_settings=0x7f070014;
+        public static final int menu_settings=0x7f070031;
+        public static final int tableLayout1=0x7f070014;
         public static final int tableRow1=0x7f070000;
         public static final int tableRow2=0x7f070005;
         public static final int tableRow3=0x7f07000a;
         public static final int tableRow4=0x7f07000f;
+        public static final int tableRow5=0x7f070019;
+        public static final int tableRow6=0x7f07001f;
+        public static final int tableRow7=0x7f070025;
+        public static final int tableRow8=0x7f07002b;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
+        public static final int activity_main_activity2=0x7f030001;
     }
     public static final class menu {
         public static final int activity_main=0x7f060000;
+        public static final int activity_main_activity2=0x7f060001;
     }
     public static final class string {
         public static final int app_name=0x7f040000;
         public static final int hello_world=0x7f040001;
         public static final int menu_settings=0x7f040002;
         public static final int title_activity_main=0x7f040003;
+        public static final int title_activity_main_activity2=0x7f040004;
     }
     public static final class style {
         public static final int AppTheme=0x7f050000;
